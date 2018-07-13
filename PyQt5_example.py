68fc02769e0b7a5a25e8c4198e7f8874e99f0ea6
Bennett Hawley <bennetthawley@gmail.com>
2018-07-13 10:57:00 -0400

    Added context menu

--- PyQt5_example.py
+++ PyQt5_example.py
@@ -1,7 +1,6 @@
 import sys
 
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication
+from PyQt5.QtWidgets import QMainWindow, qApp, QMenu, QApplication
 
 
 class Example(QMainWindow):
@@ -13,35 +12,20 @@ class Example(QMainWindow):
 
     def initUI(self):
 
-        self.statusbar = self.statusBar()
-        self.statusbar.showMessage('Ready')
-
-        menubar = self.menuBar()
-        view_menu = menubar.addMenu('View')
-
-        view_status_action = QAction('View statusbar', self, checkable=True)
-        view_status_action.setStatusTip('View statusbar')
-        view_status_action.setChecked(True)
-        view_status_action.triggered.connect(self.toggleMenu)
-
-        view_menu.addAction(view_status_action)
-
-        import_menu = QMenu('Import', self)
-        import_action = QAction('Import mail', self)
-        import_menu.addAction(import_action)
-
         self.setGeometry(300, 300, 300, 200)
         self.setWindowTitle('Simple menu')
-        self.setWindowIcon(QIcon('web.png'))
-
         self.show()
 
-    def toggleMenu(self, state):
+    def contextMenuEvent(self, event):
+        context_menu = QMenu(self)
 
-        if state:
-            self.statusbar.show()
-        else:
-            self.statusbar.hide()
+        new_action = context_menu.addAction("New")
+        open_action = context_menu.addAction("Open")
+        quit_action = context_menu.addAction("Quit")
+        action = context_menu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == quit_action:
+            qApp.quit()
 
 if __name__ == '__main__':
 
