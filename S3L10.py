30b893445837a0453df25a45fe41fca42cc43669
Bennett Hawley <bennetthawley@gmail.com>
2018-08-29 14:35:35 -0400

    Working with layout widgets in Section 3 Lesson 10

---
+++ S3L10.py
@@ -0,0 +1,53 @@
+# Only needed for access to command line arguments
+import sys
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+class Color(QWidget):
+
+    def __init__(self, color, *args, **kwargs):
+        super(Color, self).__init__(*args, **kwargs)
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
+
+
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("My Awesome App")
+
+        tabs = QTabWidget()
+        tabs.setDocumentMode(True)
+        tabs.setTabPosition(QTabWidget.East)
+        tabs.setMovable(True)
+
+        for n, color in enumerate(['red', 'green', 'blue', 'yellow']):
+            tabs.addTab(Color(color), color)
+
+        self.setCentralWidget(tabs)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+
+
+# You need one (and only one) QApplication instance per application.
+# Pass in sys.argv to allow command line arguments for your app.
+# If you know you won't use command line arguments QApplication([]) works too.
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+
+# Start the event loop.
+app.exec_()
+
+# Your application won't reach here until you exit and the event
+# loop has stopped.
